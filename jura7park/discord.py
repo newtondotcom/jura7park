732164679bd2eade009ec1d23c998d075133bab0
newtondotcom <asphalt9fr@gmail.com>
2024-12-22 23:34:54 +0100

    feat: add main functions to app

---
+++ jura7park/discord.py
@@ -0,0 +1,33 @@
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+def send_achat(name,prix,objet):
+    url = 'WEBHOOK URL'
+    message = str(name) + " a acheté " + str(objet) + " pour " + str(prix) + " points"
+    webhook = DiscordWebhook(url=url,content=message)
+    # you can set the color as a decimal (color=242424) or hex (color='03b2f8') number
+    embed = DiscordEmbed(title='Achat de ' +  name, description= objet + ' pour '+ str(prix), color='03b2f8')
+    # add embed object to webhook
+    webhook.add_embed(embed)
+    webhook.execute()
+
+def send_qr(name,cle,point):
+    url = 'WEBHOOK URL'
+    message = str(name) + " a utilisé le code QR " + str(cle) + " pour " + str(point) + " points"
+    webhook = DiscordWebhook(url=url,content=message)
+    embed = DiscordEmbed(title='Activation de' + name, description='Code : '+cle+ ' qui rapporte '+ str(point), color='03b2f8')
+    webhook.add_embed(embed)
+    webhook.execute()
+
+def send_message(message):
+    url = 'WEBHOOK URL'
+    webhook = DiscordWebhook(url=url,content=message)
+    webhook.execute()
+
+def send_defi(payeur,beneficiaire,defi,montant):
+    url = 'WEBHOOK URL'
+    message = str(payeur) + " a validé le défi " + str(defi) + " pour " + str(beneficiaire) + " avec "+ str(montant) + " points"
+    webhook = DiscordWebhook(url=url,content=message)
+    embed = DiscordEmbed(title='Validation du défi' + str(defi), description= str(montant) + " pour " + str(beneficiaire), color='03b2f8')
+    webhook.add_embed(embed)
+    webhook.execute()
--- jura7park/urls.py
+++ jura7park/urls.py
@@ -1,22 +1,81 @@
-"""
-URL configuration for jura7park project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from . import views
+from django.urls import path, include
 
 urlpatterns = [
+
+    path('ping', views.ping, name='ping'),
+    path('shop/<nb>', views.shop),
+    path('ld',views.leaderboard),
+    path('enigmes',views.enigmelist),
+    path('enigme/<nb>',views.enigme),
+    path('repenigme',views.repenigme),
+    path('registerbet/<id>',views.registerbet),
+    path('bet',views.tempo),
+    path('revealdino',views.revealdino),
+    path('remerciements', views.remerciements),
+    ##admin panel
     path('admin/', admin.site.urls),
+    #path('api',include(router.urls)),
+    path('photos', views.showphotos),
+    ##where an admin can create a code qr
+    path('createcodeqr',views.createcodeqr),
+    ##where an admin shows the code qr created
+    path('codeqrcreated',views.codeqrcreated),
+    ##where a user can validate a buying
+    path('validate/<name>',views.validate),
+    ##where a user sees that is buying is confirmed
+    path('validatedbuying/<name2>',views.validatedbuying),
+    ##where a user validates a code
+    path('validateqrcode/<name>',views.validateqrcode),
+    ##where a user sees that is qrcode is confirmed
+    path('validatedqrcode/<name2>',views.validatedqrcode),
+    ##my account page
+    path('myaccount',views.my_account),
+
+    ##Where to register an user as a staff
+    path('registerstaff/<name>',views.registerstaff),
+    ##Where to register an user as a admin
+    path('registeradmin/<name>',views.registeradmin),
+    ##Where to unregister an user as a staff
+    path('unregisterstaff/<name>',views.unregisterstaff),
+    ##Show the page nb of saved avatars
+    path('avatarlist/<nb>',views.genavatarlist),
+    ##Define the current avatar
+    path('chooseavatar/<nb>',views.chooseavatar),
+    ##Reset balance
+    path('resetbalance',views.resetbalance),
+    ##Update the balance of the user blaze with the value points for the reason defi
+    path('ub/<blaze>/<points>/<defi>',views.ub),
+    ##Adjust the balance of the user blaze with the value points for the reason raison
+    path('adjust/<blaze>/<points>/<raison>',views.adjust),
+    ##Shows the buying list
+    path('achats',views.achats),
+    ##Shows the qrcode list
+    path('codes',views.codes),
+    ##Shows the qrcode list
+    path('defis',views.defis),
+    ##Pay Monday Bet
+    path('paylundi',views.paylundi),
+    ##Pay users for bet n1
+    path('paybet1/<winner>/<cote>',views.paybet1),
+    ##Pay users for bet n2
+    path('paybet2/<winner>/<cote>',views.paybet2),
+
+    ###Notifications
+    path('webpush/', include('webpush.urls')),
+    path('testnotif', views.testnotif),
+    path('sendnotif', views.sendnotif),
+    #path('sw.js', TemplateView.as_view(template_name='slistener.js', content_type='application/x-javascript')),
+
+    ###PWA
+    path('offline', views.offline),
+
+    ###MUST BE AT THE END
+    path('', include('pwa.urls')),
+
 ]
+
+admin.site.site_header  =  "Jura7Park"
+admin.site.site_title  =  "Jura7Park Admin"
+admin.site.index_title  =  "Welcome to Jura7Park Admin"
