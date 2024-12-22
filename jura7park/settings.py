4c6c6bad52c2bc8f8cd0ae5d34d15c395cdf4d37
newtondotcom <asphalt9fr@gmail.com>
2024-12-23 00:04:35 +0100

    chore: remove webpush try

--- jura7park/settings.py
+++ jura7park/settings.py
@@ -3,20 +3,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-g!w2fo(2h*1h-@@0-wpan3hs9cv41p_(7a04(el66tw$=q@ksf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -26,7 +19,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'db.apps.DbConfig',
-    'webpush',
     'pwa'
 ]
 
